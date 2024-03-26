46d878e6ccabe5abe59cc44700ecc0b343eefe36
Mathieu Laparie <mathieu.laparie@inrae.fr>
2024-03-26 03:02:00 +0100

    Add plot to check missing values over the last 24h in R pipeline, fix a typo in yoctofzf

--- pipelines/i2c-long-format.R
+++ pipelines/i2c-long-format.R
@@ -7,7 +7,7 @@ suppressMessages(library(plotly))
 suppressMessages(library(lubridate))
 suppressMessages(library(wesanderson))
 suppressMessages(library(htmlwidgets))
-suppressMessages(library(UpSetR))
+# suppressMessages(library(UpSetR))
 
 # Get the arguments passed to the script
 args <- commandArgs(trailingOnly = TRUE)
@@ -22,7 +22,7 @@ folder <- args[1]
 # vh4w <- readLines(con="stdin", 1)
 # Else set the variable manually:
 # vh4w <- "v02"
-# df <- fread("~/Projects/yocto-utils/data/v02/joined/joined_filtered.csv", sep = ",")
+# df <- fread("~/Projects/yocto-utils/data/v02.bak/joined/joined_filtered.csv", sep = ",")
 
 # Load data
 df <- fread(file_arg, sep = ",")
@@ -53,6 +53,12 @@ df <- df %>% mutate(yday = yday(datetime)) %>%
                              "18" = "T3",
                              "19" = "RH3",
                              "20" = "L3"))) %>%
+    mutate(variable = factor(variable, levels = c("Voltage", "T1", "RH1", "L1",
+                                                  "NTC1", "NTC2", "NTC3", "NTC4",
+                                                  "NTC5", "NTC6", "NTC7", "NTC8",
+                                                  "NTC9", "NTC10",
+                                                  "T2", "RH2", "L2",
+                                                  "T3", "RH3", "L3"))) %>%
     mutate(type = case_when(
                variable %in% c("Voltage") ~ "Voltage",
                variable %in% c("T1", "T2", "T3") ~ "Temperature",
@@ -82,7 +88,7 @@ df_full <- left_join(all_combinations, df, by = c("node", "variable", "type"))
 # Round datetime to 3 minutes (which is longer than a full station wake) and fill in
 # NA for 'datetime' and 'value' where 'type' is present but 'datetime' and 'value' are missing
 df_rounded <- df_full %>%
-    mutate(datetime = round_date(datetime, unit = "3 minutes")) %>%
+    mutate(datetime = floor_date(datetime, unit = "30 minutes")) %>%
     mutate(datetime = if_else(is.na(datetime) & !is.na(type), as.POSIXct(NA), datetime),
            value = if_else(is.na(value) & !is.na(type), NA_real_, value))
 
@@ -91,25 +97,67 @@ df_rounded <- df_full %>%
 setwd(folder)
 
 # ggplot
-g1 <- df_rounded %>%
-  ggplot(aes(x = datetime, y = value, color = variable)) +
-  geom_point(alpha = 0.7, size = 0.7, aes(text = paste(datetime, "<br>", variable, " = ", value, sep = ""))) +
-  geom_line(alpha = 0.7, lwd = 0.2) +
-  scale_color_viridis(end = 0.9, option = "C", discrete = TRUE) +
-  facet_grid(type ~ node, scales = "free_y") +
-  theme(axis.text.x = element_text(angle = -45, vjust = 0.5, hjust=-0, size = 7), legend.title = element_blank()) +
-  xlab("") + ylab("")
+# Overview in node x type facets
+overview <- df_rounded %>%
+    ggplot(aes(x = datetime, y = value, color = variable)) +
+    geom_point(alpha = 0.7, size = 0.7, aes(text = paste(datetime, "<br>", variable, " = ", value, sep = ""))) +
+    geom_line(alpha = 0.7, lwd = 0.2) +
+    scale_color_viridis(end = 0.9, option = "C", discrete = TRUE) +
+    facet_grid(type ~ node, scales = "free_y") +
+    theme(axis.text.x = element_text(angle = -45, vjust = 0.5, hjust=-0, size = 7), legend.title = element_blank()) +
+    xlab("") + ylab("")
 
 # Convert to ggplotly and specify tooltip content
-overview <- ggplotly(g1, tooltip = "text")
+overview <- ggplotly(overview, tooltip = "text")
 overview$x$layout$legend$title$text <- ""
 overview
-
-# Save the Plotly widget as an HTML file
 saveWidget(overview, "overview.html", selfcontained = TRUE)
 
 # Plotly
-# Plotly subplots for type with node legends
+# Overview with type facets
+summary <- df %>% group_by(type) %>%
+    mutate(hour = hour(datetime)) %>%
+    group_map(~ plot_ly(.,
+    x = ~datetime, 
+    y = ~value, 
+    color = ~variable, # ~node,
+    alpha = 0.5,
+#    frame = ~yday,
+    text = ~paste0("N", node, ", ", variable),
+    hoverinfo = 'text',
+    type = 'scatter',
+    mode = 'markers'),
+    keep = TRUE) %>%
+    subplot(nrows = 2, shareX = TRUE, shareY = FALSE, titleX = FALSE) %>%
+    layout(plot_bgcolor='#e5ecf6',
+           xaxis = list(
+               rangeselector = list(
+                   buttons = list(
+                       list(
+                           count = 1,
+                           label = "1 d",
+                           step = "day",
+                           stepmode = "backward"),
+                       list(
+                           count = 1,
+                           label = "1 w",
+                           step = "week",
+                           stepmode = "backward"),
+                       list(
+                           count = 2,
+                           label = "2 w",
+                           step = "week",
+                           stepmode = "backward"),
+                       list(
+                           count = 3,
+                           label = "3 mo",
+                           step = "month",
+                           stepmode = "backward"),
+                       list(step = "all")))))
+summary
+saveWidget(summary, "summary.html", selfcontained = TRUE)
+
+# Plotly subplots timeseries for type, with node legends
 types <- levels(df$type)
 plots <- lapply(types, function(t) {
   # Filter the dataframe for the current type
@@ -122,15 +170,16 @@ plots <- lapply(types, function(t) {
       x = ~datetime,
       y = ~value,
       color = ~node,
-      text = ~paste(datetime, "<br>", "Node ", node, "<br>", variable, " = ", value, sep = ""),
+      alpha = 0.7,
+      text = ~paste(datetime, "<br>", "N", node, "<br>", variable, " = ", value, sep = ""),
       hoverinfo = 'text',
       type = 'scatter',
-      mode = 'lines+markers',
+      mode = 'markers',
       height = 1500,
       legendgroup = ~node,
       showlegend = ~ifelse(t == "Temperature", TRUE, FALSE),
-      line = list(width = 2, opacity = 0.7),
-      marker = list(size = 5, opacity = 0.7),
+#      line = list(width = 2, opacity = 0.5),
+      marker = list(size = 6, opacity = 0.7),
 #      colors = RColorBrewer::brewer.pal(8, "Set3")
       colors = c(wes_palette("GrandBudapest1"), wes_palette("GrandBudapest2"))
     ) %>%
@@ -149,79 +198,101 @@ plots <- lapply(types, function(t) {
   return(p)
 })
 
-timeseries <- subplot(plots, nrows = length(types), shareX = TRUE, shareY = FALSE) %>% layout(
-    plot_bgcolor='#e5ecf6',
-    xaxis = list(
-        rangeselector = list(
-            buttons = list(
+timeseries <- subplot(plots, nrows = length(types), shareX = TRUE, shareY = FALSE) %>%
+    layout(
+        plot_bgcolor='#e5ecf6',
+        xaxis = list(
+            rangeselector = list(
+                buttons = list(
+                    list(
+                        count = 4,
+                        label = "4 h",
+                        step = "hour",
+                        stepmode = "backward"),
+                    list(
+                        count = 1,
+                        label = "1 d",
+                        step = "day",
+                        stepmode = "backward"),
+                    list(
+                        count = 1,
+                        label = "1 w",
+                        step = "week",
+                        stepmode = "backward"),
+                    list(
+                        count = 2,
+                        label = "2 w",
+                        step = "week",
+                        stepmode = "backward"),
+                    list(
+                        count = 3,
+                        label = "3 mo",
+                        step = "month",
+                        stepmode = "backward"),
+                    ## list(
+                    ##     count = 1,
+                    ##     label = "YTD",
+                    ##     step = "year",
+                    ##     stepmode = "todate"),
+                    list(step = "all"))),
+            rangeslider = list(type = "date",
+                               bgcolor = "#EBEBEB",
+                               thickness = "0.05")),
+        updatemenus = list(
             list(
-                count = 4,
-                label = "4 h",
-                step = "hour",
-                stepmode = "backward"),
-            list(
-                count = 1,
-                label = "1 d",
-                step = "day",
-                stepmode = "backward"),
-            list(
-                count = 1,
-                label = "1 w",
-                step = "week",
-                stepmode = "backward"),
-            list(
-                count = 2,
-                label = "2 w",
-                step = "week",
-                stepmode = "backward"),
-            list(
-                count = 3,
-                label = "3 mo",
-                step = "month",
-                stepmode = "backward"),
-            ## list(
-            ##     count = 1,
-            ##     label = "YTD",
-            ##     step = "year",
-            ##     stepmode = "todate"),
-            list(step = "all"))),
-        rangeslider = list(type = "date",
-                           bgcolor = "#EBEBEB",
-                           thickness = "0.05")),
-    updatemenus = list(
-        list(
-            x = Inf,
-            y = Inf,
-            buttons = list(
-                list(method = "restyle",
-                     args = list("mode", "lines+markers"),
-                     label = "Lines + markers"),
-                list(method = "restyle",
-                     args = list("mode", "markers"),
-                     label = "Markers"),
-                list(method = "restyle",
-                     args = list("mode", "lines"),
-                     label = "Lines")))))
+                x = Inf,
+                y = Inf,
+                buttons = list(
+                    list(method = "restyle",
+                         args = list("mode", "markers"),
+                         label = "Markers"),
+                    list(method = "restyle",
+                         args = list("mode", "lines+markers"),
+                         label = "Lines + markers"),
+                    list(method = "restyle",
+                         args = list("mode", "lines"),
+                         label = "Lines")))))
 timeseries
 saveWidget(timeseries, "timeseries.html", selfcontained = TRUE)
 
-# Overview with type legends
-## summary <- df %>% group_by(type) %>%
-##     mutate(hour = hour(datetime)) %>%
-##     do(p = plot_ly(.,
-##     x = ~datetime, 
-##     y = ~value, 
-##     color = ~variable, 
-## #    frame = ~node,
-##     text = ~node,
-##     hoverinfo = 'text',
-##     type = 'scatter',
-##     mode = 'markers')) %>%
-##     subplot(nrows = 1, shareX = TRUE, shareY = FALSE)
-## summary
-## saveWidget(summary, "summary.html", selfcontained = TRUE)
+## Missing data
+# geom_raster
+present <- df %>%
+    select(-yday, -second, -variable.orig, -type) %>%
+    filter(datetime >= last(datetime) - dhours(24)) %>%
+    mutate(datetime = floor_date(datetime, "30 minutes") - dminutes(0)) %>%
+    group_by(datetime, node, variable) %>%
+    # Summarize each group: 1 if there's at least one non-NA value, 0 or NA otherwise
+    summarise(value = value,
+              presence = as.factor(ifelse(any(!is.na(value)), "Received", NA_real_)), .groups = 'drop')
 
-## # Check missing data
+missingplot <- present %>%
+    ggplot(aes(x = variable, y = datetime, fill = variable)) +
+    geom_raster(aes(text = paste(datetime, "<br>Node ", node, "<br>", variable, " = ",
+                                 ifelse(presence == "Received", value, " missing"), sep = ""))) +
+    scale_fill_grey(name = "", end = 0.6) +
+    labs(y = "Last 24 hours split in 30-min tiles starting at --:00",
+         x = "",
+         title = "Wake ups every 30 min (--:-0) for 3 min (i.e., within single tiles)") +
+    theme(axis.text.x = element_text(size = 6, angle =-55,
+                                     hjust = 0, vjust = 1),
+          text = element_text(family = "monospace", size = 8),
+          panel.spacing = unit(1, "lines"),
+          panel.background = element_rect(fill = "white")) +
+    coord_fixed(1/1000) +
+    scale_y_datetime(expand = c(0, 0)) +
+    scale_x_discrete(expand = c(0, 0)) +
+    facet_wrap(. ~ node, ncol = 7)
+missingplotly <- ggplotly(missingplot)
+
+# Convert to ggplotly and specify tooltip content
+missingplotly <- ggplotly(missingplot, tooltip = "text")
+missingplotly$x$layout$legend$title$text <- ""
+missingplotly %>% layout(autosize = FALSE, width = 1000, height = 800)
+missingplotly
+saveWidget(missingplotly, "missing.html", selfcontained = TRUE)
+
+## # Using UpSetR
 ## df_wide_rounded <- df_rounded %>% select(-c(variable.orig, type)) %>%
 ##     pivot_wider(names_from = variable,
 ##                 values_from = value) %>%
@@ -245,3 +316,7 @@ saveWidget(timeseries, "timeseries.html", selfcontained = TRUE)
 ##           empty.intersections = "on",
 ##           sets.x.label = "Number of values",
 ##           mainbar.y.label = "Number of missing combinations")
+
+## # Python, missingno
+# df_rounded %>% select(-c("variable.orig")) %>%
+#    write.csv("data_for_missingno.csv", row.names = FALSE)
